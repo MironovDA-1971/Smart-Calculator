1c4928224707d87c9506ffc6d0931dbd8c99ba01
MironovDA-1971 <mironovda@gmail.com>
2022-11-28 15:05:36 +0300

    Update classes.kt

--- classes.kt
+++ classes.kt
@@ -3,47 +3,33 @@ package calculator
 object Input {
 
     fun checkInput() {
-
         while (true) {
-            val numString = readLine()?.trim()
-                ?.replace("\\s{2,}".toRegex(), " ")
-                ?.replace("[+]{2,}".toRegex(), "+")
-
-            when (numString) {
+            when (val numString = readLine()?.trim()) {
                 "/exit" -> break
                 "/help" -> println("The program calculates the sum of numbers")
                 "" -> continue
-                else -> if (numString != null) regexStr(numString)
+                else -> if (numString != null) plusMinus(numString)
             }
         }
     }
 
-    private fun plusMinus (numString: String) {
-        val outString = numString.split(" ").toMutableList()
+    private fun plusMinus(numString: String) {
+        var numList = List(0){""}
+        if ("/.*".toRegex().matches(numString)) println("Unknown command")
+        else {
+            numList = numString
+                .replace("\\s{2,}".toRegex(), " ")
+                .replace("--".toRegex(), "+")
+                .replace("[+]{2,}".toRegex(), "+")
+                .replace("\\+- ".toRegex(), "+ -")
+                .replace(" - ".toRegex(), " + -")
+                .split(" + ")
+        }
         try {
-            var result = outString[0].toInt()
-            for (i in outString.indices) {
-                if ("--" in outString[i]) {
-                    if (outString[i].length % 2 == 0) outString[i] = "+" else outString[i] = "-"
-                }
-                if (outString[i] == "+") result += outString[i + 1].toInt()
-                else if (outString[i] == "-") result -= outString[i + 1].toInt()
-            }
+            var result = numList.sumOf { it.trim().toInt() }
             println(result)
         } catch (e: Exception) {
             println("Invalid expression")
         }
     }
-
-    private fun regexStr(numString: String) {
-        if ("/.*".toRegex().matches(numString)) println("Unknown command")
-        else if (
-            "\\s?[-+]?\\d+\\s[+-]*".toRegex().containsMatchIn(numString) ||
-            "\\s?[-+]?\\d+".toRegex().matches(numString)
-        ) {
-            plusMinus(numString)
-        }
-        else println("Invalid expression")
-    }
-
 }
