3d69e0dee1fe1379259cfb53a7c47a6092038ef7
MironovDA-1971 <mironovda@gmail.com>
2022-11-29 03:45:35 +0300

    Ready Stage 6/8: Variables

--- classes.kt
+++ classes.kt
@@ -1,6 +1,7 @@
 package calculator
 
 object Input {
+    private val mapList: MutableMap<String,String> = mutableMapOf()
 
     fun checkInput() {
         while (true) {
@@ -15,12 +16,46 @@ object Input {
 
     private fun testString(numString: String) {
         if ("/.*".toRegex().matches(numString)) println("Unknown command")
-        else if("^[a-zA-Z]+.*".toRegex().matches(numString)) checkVal(numString)
+        else if("^[a-zA-Z]+.*".toRegex().matches(numString)) checkVar(numString.trim())
         else plusMinus(numString)
     }
 
-    private fun checkVal(numString: String) {
-        if("^[a-zA-Z]+\\s*=\\s*\\d+".toRegex().matches(numString)){}
+    private fun checkVar(numString: String) {
+        var uvFlag = true
+        var iaFlag = true
+        var iiFlag = true
+        var setFlag = listOf<Boolean>()
+        var newStr = ""
+        if(".*=.*".toRegex().matches(numString)){
+            val (b,c) = numString.split("=")
+            if(!"[a-zA-Z]+".toRegex().matches(b.trim())) iiFlag = false
+            else if(
+                !"[a-zA-Z]+".toRegex().matches(c.trim()) &&
+                !"\\d+".toRegex().matches(c.trim())
+            ) iaFlag = false
+            else if (mapList.containsKey(c.trim())) mapList += b.trim() to mapList[c.trim()].toString()
+            else if ("\\d+".toRegex().matches(c.trim())) mapList += b.trim() to c.trim()
+            else  uvFlag = false
+            //println(mapList)
+        }
+        else if (".*[-|+].*".toRegex().matches(numString)) {
+            numString.split(" ").toSet().mapNotNull {
+                if ("[a-zA-Z]+".toRegex().matches(it)) setFlag = listOf(it in mapList.keys)
+            }
+            if (false !in setFlag) {
+                numString.split(" ").map {
+                        newStr += if ("[a-zA-Z]+".toRegex().matches(it))  "${mapList[it]} "
+                        else "$it "
+                    }
+                plusMinus(newStr)
+            }
+        }
+        else if (numString !in mapList.keys) uvFlag = false
+        else println(mapList[numString])
+
+        if (!iiFlag) println("Invalid identifier")
+        if (!iaFlag) println("Invalid assignment")
+        if (!uvFlag || (false in setFlag)) println("Unknown variable")
     }
 
     private fun plusMinus(numString: String) {
